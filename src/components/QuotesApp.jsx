eedb709a50cbd0ee4b097d98e6c6b9eccb87c978
Ritu Bafna <ritujane78@gmail.com>
2026-06-17 18:48:35 +0545

    create template for the quotes card

--- src/components/QuotesApp.jsx
+++ src/components/QuotesApp.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const QuotesApp = () => {
   return (
-    <div>
-      Quotes
+    <div className='container'>
+        <div className="quotes-app">
+            <div className="app-heading">Quote.</div>
+            <i className="bx bxs-heart fav-icon"></i>
+            <div className="quote">
+                <i className="bx bxs-quote-alt-left left-quote"></i>
+                <p className="quote-text">Ask not what your country can do for you but what you can do for your country.</p>
+                <p className='quote-author'>John Kennedy</p>
+                <i className="bx bxs-quote-alt-right right-quote"></i>
+            </div>
+            <div className="circles">
+                <div className="circle-1"></div>
+                <div className="circle-2"></div>
+                <div className="circle-3"></div>
+                <div className="circle-4"></div>
+            </div>
+            <div className="buttons">
+                <button className="btn btn-new">New Quote</button>
+                <button className="btn btn-fav">Add to Favorites</button>
+            </div>
+        </div>
     </div>
   )
 }
