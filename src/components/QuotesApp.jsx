1025a67a7e9f4d39324413f011cc704ba49efcc8
Ritu Bafna <ritujane78@gmail.com>
2026-06-18 12:11:02 +0545

    create favorites template

--- src/components/QuotesApp.jsx
+++ src/components/QuotesApp.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const QuotesApp = () => {
+const [quote, setQuote] = useState({
+    text: "Ask not what your country can do for you but what you can do for your country.",
+    author: "John Kennedy"
+})
+const fetchQuote = async () => {
+    const url = "https://api.quotable.io/random";
+    const res = await fetch(url);
+    const data = await res.json();
+    setQuote({
+        text: data.content,
+        author: data.author
+    })
+}
   return (
     <div className='container'>
         <div className="quotes-app">
@@ -8,8 +21,8 @@ const QuotesApp = () => {
             <i className="bx bxs-heart fav-icon"></i>
             <div className="quote">
                 <i className="bx bxs-quote-alt-left left-quote"></i>
-                <p className="quote-text">Ask not what your country can do for you but what you can do for your country.</p>
-                <p className='quote-author'>John Kennedy</p>
+                <p className="quote-text">{quote.text}</p>
+                <p className='quote-author'>{quote.author}</p>
                 <i className="bx bxs-quote-alt-right right-quote"></i>
             </div>
             <div className="circles">
@@ -19,8 +32,22 @@ const QuotesApp = () => {
                 <div className="circle-4"></div>
             </div>
             <div className="buttons">
-                <button className="btn btn-new">New Quote</button>
+                <button className="btn btn-new" onClick={fetchQuote}>New Quote</button>
                 <button className="btn btn-fav">Add to Favorites</button>
+            </div>
+            <div className="favorites">
+                <button className="btn-close">
+                    <i className="bx bx-x"></i>
+                </button>
+                <div className="fav-quote">
+                    <div className="fav-quote-delete">
+                        <i className="bx bx-x-circle"></i>
+                    </div>
+                    <div className="fav-quote-content">
+                        <div className="fav-quote-text">Ask not what your country can do for you but what you can do for your country.</div>
+                        <div className="fav-quote-author">John Kennedy</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
